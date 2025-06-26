f945c8c36e6603483b880241062037639157f121
calvinw <calvin.e.williamson@gmail.com>
2025-06-26 01:06:28 -0400

    fixing run chainlit

--- mcp-greet/run_chainlit.sh
+++ mcp-greet/run_chainlit.sh
@@ -8,5 +8,10 @@ if [ ! -f "app.py" ]; then
     wget --clobber https://raw.githubusercontent.com/calvinw/chainlit-mcp-client/refs/heads/main/chainlit_mcp_client/app.py
 fi
 
+if [ ! -f "chainlit.md" ]; then
+    echo "📋 Getting chainlit.md..."
+    wget --clobber https://raw.githubusercontent.com/calvinw/chainlit-mcp-client/refs/heads/main/chainlit_mcp_client/chainlit.md
+fi
+
 uv sync
 uv run chainlit run -w app.py --host 0.0.0.0 --port "$PORT"

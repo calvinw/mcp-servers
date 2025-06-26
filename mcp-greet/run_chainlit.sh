6303fff6627ba22d85a464f270adb04c896c6a28
calvinw <calvin.e.williamson@gmail.com>
2025-06-26 01:39:52 -0400

    adding the config.toml

--- mcp-greet/run_chainlit.sh
+++ mcp-greet/run_chainlit.sh
@@ -13,5 +13,15 @@ if [ ! -f "chainlit.md" ]; then
     wget --clobber https://raw.githubusercontent.com/calvinw/chainlit-mcp-client/refs/heads/main/chainlit_mcp_client/chainlit.md
 fi
 
+if [ ! -d ".chainlit" ]; then
+    echo "📁 Creating .chainlit directory..."
+    mkdir -p .chainlit
+fi
+
+if [ ! -f ".chainlit/config.toml" ]; then
+    echo "⚙️ Getting config.toml..."
+    wget --clobber https://raw.githubusercontent.com/calvinw/chainlit-mcp-client/refs/heads/main/chainlit_mcp_client/.chainlit/config.toml -O .chainlit/config.toml
+fi
+
 uv sync
 uv run chainlit run -w app.py --host 0.0.0.0 --port "$PORT"
